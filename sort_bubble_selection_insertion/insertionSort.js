fbcf55ace4e63c0d8e3c1b67f2b627f9b7742aa4
faith7 <faithdevelop@gmail.com>
2019-04-16 11:26:12 -0500

    Found bugs in insertion sort(Section13). Updated with index tracker

--- sort_bubble_selection_insertion/insertionSort.js
+++ sort_bubble_selection_insertion/insertionSort.js
@@ -1,20 +1,16 @@
-function insertion(arr){
+function insertion(arr) {
 
-    let swap = (i, j, arr) => ([arr[i],arr[j]]=[arr[j], arr[i]]);
-    for(let i=0; i<arr.length; i++){
-        for(let j=arr.length-1; j>=i; j--){
-            if(arr[i] > arr[j]){
-                swap(i, j, arr);
-                while(arr[i]>arr[i+1]){
-                    swap(arr[i], arr[i+1]);
-                } 
-
-            }
+    let swap = (i, j, arr) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        for (var j = i - 1; j >= 0 && arr[j] > current; j--) {
+            arr[j + 1] = arr[j];
         }
+        arr[j + 1] = current;
     }
-    return arr; 
+    return arr;
 }
 console.log(insertion([1, 2, 3, 4, 6, 5]));
 console.log(insertion([1, 5, 3, 7, 9]));
 console.log(insertion([9, 6, 3, 2, 15]));
-console.log(insertion([10, 9, 8, 7, 15, 6])); 
+console.log(insertion([10, 9, 8, 7, 15, 6, 12])); 
